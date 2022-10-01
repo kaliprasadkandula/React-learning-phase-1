6ada4da2342dd979c5f72ba7a8f3acfd61828363
TSV Kali prasad K <kaliprasad4035@gmail.com>
2022-10-01 19:43:29 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function NameBlock({nameForClass,name,income=0}){ //with the help of destructuring we can pass default values 
+  return <div nameForClass={nameForClass}>{name} has income {income}</div>
+}
+function App() {  //App is just a component
+  return <div>
+    Hi
+    <NameBlock name={"kali"} nameForClass={"div1"} income={200}/>
+    <NameBlock name={"teja"} nameForClass={"div2"} />
+    <NameBlock name={"sai"}  nameForClass={"div3"} income={1000000}/>
+    learners
+    </div>;
 }
 
 export default App;
